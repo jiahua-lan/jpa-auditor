d601bb8aa31e744dd01f05d6c7966fb8b160d18b
jiahua-lan <jiahua_lan@outlook.com>
2019-09-22 14:56:21 +0800

    补全CRUD操作

--- src/main/java/com/miaostar/auditor/handler/DocumentHandler.java
+++ src/main/java/com/miaostar/auditor/handler/DocumentHandler.java
@@ -2,15 +2,19 @@ package com.miaostar.auditor.handler;
 
 import com.miaostar.auditor.entity.Document;
 import com.miaostar.auditor.repository.DocumentRepository;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/documents")
 public class DocumentHandler {
     private DocumentRepository documentRepository;
 
@@ -19,7 +23,7 @@ public class DocumentHandler {
     }
 
     @PreAuthorize("hasAuthority('D0001')")
-    @PostMapping(name = "创建文档", value = "/documents")
+    @PostMapping(name = "创建文档")
     public HttpEntity<?> create(@RequestBody Document document) {
         Document entity = documentRepository.save(document);
         return ResponseEntity
@@ -28,10 +32,36 @@ public class DocumentHandler {
     }
 
     @PreAuthorize("hasAuthority('D0002')")
-    @GetMapping(name = "查找文档", value = "/documents/{id}")
+    @GetMapping(name = "查找文档", value = "/{id}")
     public HttpEntity<?> find(@PathVariable Long id) {
         Optional<Document> document = documentRepository.findById(id);
         return ResponseEntity.of(document);
+    }
+
+    @PreAuthorize("hasAuthority('D0003')")
+    @GetMapping(name = "文档列表")
+    public HttpEntity<?> findAll(@RequestBody Document document) {
+        ExampleMatcher matcher = ExampleMatcher.matching().withIgnoreNullValues();
+        Example<Document> example = Example.of(document, matcher);
+        List<Document> list = documentRepository.findAll(example);
+        return ResponseEntity.ok(list);
+    }
+
+    @PutMapping(name = "修改文档内容", value = "/{id}")
+    public HttpEntity<?> replace(@RequestBody Document document, @PathVariable("id") Long id) {
+        Optional<Document> optional = documentRepository.findById(id)
+                .map(doc -> {
+                    doc.setContent(document.getContent());
+                    doc.setTitle(document.getTitle());
+                    return documentRepository.save(doc);
+                });
+        return ResponseEntity.of(optional);
+    }
+
+    @DeleteMapping(name = "删除文档", value = "/{id}")
+    public HttpEntity<?> delete(@PathVariable("id") Long id) {
+        documentRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
